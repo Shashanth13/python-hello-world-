55d72d255ecdf0cd2433c2ea89c01db37e79ca6d
shashanth kumar <96310116+Shashanth13@users.noreply.github.com>
2023-01-17 11:38:35 +0530

    Add files via upload
    the basic codes for python

---
+++ function4.py
@@ -0,0 +1,10 @@
+def shout(text):
+    return text.upper()
+def out(text):
+    return text.lower()
+def greet(func):
+    #storing the function in a variable
+    greeting=func("Hi,I am created by a function passed as an argument.")
+    print(greeting)
+greet(shout)
+greet(out)
